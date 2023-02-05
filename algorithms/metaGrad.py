e55a7e727bc0416a52232d979592f7463dc441a1
David <mianfeiyouxiangli@163.com>
2023-02-05 21:18:20 +0800

    debugging metaGrad(unfinished)

--- algorithms/metaGrad.py
+++ algorithms/metaGrad.py
@@ -25,14 +25,13 @@ class metaAgent(BaseAgent):
         self.device = args.device
         self.episode_length = args.episode_length
         self.dim_states = 2 * env_config["num_nodes"]    # State vector's dimension
-        self.dim_actions= env_config["num_nodes"]        # Action vector's dimension
+        self.dim_actions = env_config["num_nodes"]        # Action vector's dimension
         self.max_bonus = args.max_bonus
         self.min_bonus = args.min_bonus
 
         # adjangency related parameters
         self.adj_mat = get_adj_mat(self.edge_index)
         self.neighbour_list = [np.where(self.adj_mat[i]==1)[0] for i in range(self.num_nodes)]
-        n_neighbour = np.array([neighbour.shape[0] for neighbour in self.neighbour_list])
         dim_policies = np.sum([len(neighbour) for neighbour in self.neighbour_list])
 
         # actor and critic neural network
@@ -51,7 +50,7 @@ class metaAgent(BaseAgent):
         self.batch_size = args.batch_size
         self.buffer_ptr = 0
         self.buffer = np.zeros([self.buffer_size, 2*self.dim_states+self.dim_actions+2])  # each transition is [s,a,r,s',d] where d represents don
-        self.buffer_agents_policies = np.zeros([self.buffer_size, dim_policies])    # Record history agents' actions. [buffer_size, dim_agents_policies]
+        self.buffer_agents_actions = np.zeros([self.buffer_size, dim_policies])    # Record history agents' actions. [buffer_size, dim_agents_policies]
         self.curr_agents_policies = np.zeros([self.episode_length, dim_policies])   # Record current iteration agents' policies. [episode_length, dim_agents_policies]
         self.buffer_time_steps = np.zeros([self.buffer_size])
         self.buffer_high = 0    # Used to mark the appended buffer (avoid training with un-appended sample indexes)
@@ -66,9 +65,9 @@ class metaAgent(BaseAgent):
         self.depsilon = (self.epsilon_max - self.epsilon_min) / args.decre_epsilon_episodes
         self.epsilon = self.epsilon_max
         self.is_training = True
-        self.train_steps = 0
 
         # sensitivity related constant
+        n_neighbour = np.array([neighbour.shape[0] for neighbour in self.neighbour_list])
         self.A = np.zeros([self.num_nodes, dim_policies])    # constraint matrix: only constraint that policies sums are 1
         cnt = 0
         for i in range(self.num_nodes):
@@ -76,13 +75,22 @@ class metaAgent(BaseAgent):
             cnt += n_neighbour[i]
         self.nabla_y_F = np.concatenate([np.eye(self.num_nodes)[self.neighbour_list[i]].T 
                                         for i in range(self.num_nodes)], axis=1)
+                                                # calculate the S matrix
+        n_node = env_config['num_nodes']
+        self.S = []
+        for i in range(n_node):
+            tmp = np.zeros([n_node, n_node])
+            for j in range(n_node):
+                if j in self.neighbour_list[i]:
+                    tmp[j, j] += 1
+            self.S.append(tmp)
 
     def choose_action(self, s, is_random=False):
         if is_random:
             actions = np.random.uniform(self.min_bonus, self.max_bonus, self.dim_actions)
         else:
             state = torch.FloatTensor(self.s2obs(s)).to(self.device)
-            actions = self.actor(state).cpu().data.numpy()
+            actions = self.actor(state).cpu().detach().numpy()
             actions = ( actions + np.random.normal(0, self.epsilon, self.dim_actions) ).clip(self.min_bonus, self.max_bonus)
             self.epsilon = np.maximum(self.epsilon-self.depsilon, self.epsilon_min) 
         
@@ -96,17 +104,18 @@ class metaAgent(BaseAgent):
         # Buffer_high would not exceeds so that sample_index in sample_data() would reamin in self.buffer
 
         # Record agents distribution policies
-        agent_policies = np.concatenate( [ arr[self.neighbour_list[idx]] / np.sum(arr) for idx, arr in enumerate(info[0]) ] )
-        self.buffer_agents_policies[self.buffer_ptr] = agent_policies   # Record history agents' actions. [buffer_size, dim_agents_policies]
-        self.curr_agents_policies[s['time_step']] = agent_policies  # Record current agents' policies. [episode_length, dim_agents_policies]
+        agent_actions = np.concatenate( [ arr[self.neighbour_list[idx]] / np.sum(arr)
+                                             for idx, arr in enumerate(info[0]) ] ) # [dim_policies], record the drivers' policies by vector
+        self.buffer_agents_actions[self.buffer_ptr] = agent_actions   # Record history agents' actions. [buffer_size, dim_agents_policies]
+        self.curr_agents_policies[s['time_step']] = agent_actions  # Record current agents' policies. [episode_length, dim_agents_policies]
         self.buffer_time_steps[self.buffer_ptr] = s['time_step']
         self.buffer_ptr += 1
 
     def learn(self):
         self.prep_train()
         # Sample batch
-        if self.buffer_high > self.batch_size:
-            sample_index = np.random.choice( self.buffer_high-1, self.batch_size, replace=False ) # Cannot sample replicate samples. 
+        if self.buffer_high >= self.batch_size:
+            sample_index = np.random.choice( self.buffer_high, self.batch_size, replace=False ) # Cannot sample replicate samples. 
             batch_memory = self.buffer[sample_index, :]
             batch_state = torch.FloatTensor(batch_memory[:, :self.dim_states]).to(self.device)
             # batch_actions = torch.FloatTensor(batch_memory[:, self.dim_states:self.dim_states+self.dim_actions]).to(self.device)
@@ -115,17 +124,17 @@ class metaAgent(BaseAgent):
             batch_done_np = 1 - batch_memory[:, -1]
             batch_done = torch.FloatTensor(batch_done_np).to(self.device).view([-1,1])
 
-            batch_agents_policies = self.buffer_agents_policies[sample_index]
+            batch_agents_policies = self.buffer_agents_actions[sample_index]
             batch_idle_drivers = batch_memory[:, :self.num_nodes]
             batch_demands = batch_memory[:, self.num_nodes: self.num_nodes * 2]
         else:   # Continue append transition, stop learn()
             print("Transition number is lesser than batch_size, continue training. ")
             return
 
-        next_actions = np.array([ self.curr_agents_policies[\
+        batch_next_actions = np.array([ self.curr_agents_policies[\
                                     self.buffer_time_steps[int( (i_sample+1)*batch_done_np[idx] )].astype(int) ]
                                  for idx, i_sample in enumerate(sample_index) ])
-        target_Q = self.critic_target( batch_next_state, torch.FloatTensor(next_actions).to(self.device) )
+        target_Q = self.critic_target( batch_next_state, torch.FloatTensor(batch_next_actions).to(self.device) )
         target_Q = batch_rewards + (batch_done * self.discount * target_Q).detach()
 
         # Curr q values
@@ -137,9 +146,12 @@ class metaAgent(BaseAgent):
         critic_loss.backward()
         self.critic_optim.step()
 
+        # Record maximum of critic gradient
+        critic_grad = np.concatenate([x.grad.cpu().numpy().reshape([-1]) for x in self.critic.parameters()])
+        self.writer.add_scalar("critic_grad_max", np.max(critic_grad), self.train_steps)
+        self.writer.add_scalar("critic_loss", critic_loss.item(), self.train_steps)
+
         # Update actor network
-        if 'grad' in locals().keys():
-            grad = torch.zeros_like(grad)
         for idx, state in enumerate(batch_state):
             # Try for separate update, first calculate \nabla_\theta \mu(s)
             actor_criterion = self.actor(state)
@@ -152,14 +164,14 @@ class metaAgent(BaseAgent):
 
             # Calculate the sensitivity matrix
             agents_policy = batch_agents_policies[idx]
-            L = self.get_L(agents_policy, self.adj_mat, batch_idle_drivers[idx]*NORMALIZATION_FACTOR, batch_demands[idx]*NORMALIZATION_FACTOR)
+            L = self.get_L(agents_policy, batch_idle_drivers[idx]*NORMALIZATION_FACTOR, batch_demands[idx]*NORMALIZATION_FACTOR)
             M = L-L@self.A.T@ np.linalg.inv(self.A@L@self.A.T) @ self.A@L
             nabla_y_x = (-M @ self.nabla_y_F.T).T
 
             # then calculate the \nabla_a Q(s,a)
-            self.actor_optim.zero_grad()
-            # batch_actions_mu = self.actor(state)
-            # batch_actions_mu.retain_grad()
+            # TODO: test whether zero_grad is necessary
+            # self.actor_optim.zero_grad()
+            # self.critic_optim.zero_grad()
             batch_actions_mu = torch.FloatTensor(agents_policy).to(self.device).requires_grad_(True)
             batch_actions_mu.retain_grad()
             critic_criteriion = - self.critic(state, batch_actions_mu)
@@ -171,24 +183,21 @@ class metaAgent(BaseAgent):
             else:
                 grad = actor_grads.T @ torch.FloatTensor(nabla_y_x).to(self.device) @ batch_actions_mu.grad / self.batch_size
 
-        # Reshape grads to assign the gradient to 
+        # Reshape grads to actor.parameters() shapes
         shapes = [x.shape for x in self.actor.state_dict().values()]
         shapes_prod = [torch.tensor(s).numpy().prod() for s in shapes]
         grad_split = grad.split(shapes_prod)
-        # gradient_calc = {}
         cnt = 0
         for n, p in self.actor.named_parameters():  # Assign gradient to actor network. 
-            # gradient_calc[n] = grad_split[cnt]
             p.grad = grad_split[cnt].view(p.grad.shape)
             cnt += 1
-
+        
+        # actor update
         self.actor_optim.step()
 
-        critic_grad = np.concatenate([x.grad.cpu().numpy().reshape([-1]) for x in self.critic.parameters()])
-        self.writer.add_scalar("critic_grad_max", np.max(critic_grad), self.train_steps)
+        # Record actor gradient
         actor_grad = torch.concat(grad_split)
         self.writer.add_scalar("actor_grad_max", torch.max(actor_grad).item(), self.train_steps)
-        self.writer.add_scalar("critic_loss", critic_loss.item(), self.train_steps)
         self.train_steps += 1
 
         # Soft update the target network
@@ -198,31 +207,21 @@ class metaAgent(BaseAgent):
         for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
             target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
-    def get_L(self, policies, adj_matrix, idle_drivers, demands):
+    def get_L(self, policies, idle_drivers, demands):
         """ This function calculate the L matrix """
         n_node = idle_drivers.shape[0]
-        neighbour_list = [np.where(adj_matrix[i]==1)[0] for i in range(n_node)]
-        n_neighbour = np.array([neighbour.shape[0] for neighbour in neighbour_list])
+        n_neighbour = np.array([neighbour.shape[0] for neighbour in self.neighbour_list])
         dim_policies = np.sum(n_neighbour)
-
-        # calculate the S matrix
-        S = []
-        for i in range(n_node):
-            tmp = np.zeros([n_node, n_node])
-            for j in range(n_node):
-                if j in neighbour_list[i]:
-                    tmp[j, j] += 1
-            S.append(tmp)
 
         L = np.zeros([dim_policies, dim_policies])
         cnt_col = 0
         for i in range(n_node):
             cnt_row = 0
             for j in range(n_node):
-                sub_mat = (1+(i==j)) * idle_drivers[j] * S[j][neighbour_list[j]][:, neighbour_list[i]]
+                sub_mat = (1+(i==j)) * idle_drivers[i] * self.S[j][self.neighbour_list[j]][:, self.neighbour_list[i]]
                 L[cnt_row:cnt_row+n_neighbour[j], cnt_col:cnt_col+n_neighbour[i]] = sub_mat
                 cnt_row += n_neighbour[j]
-            for idx, neighbour_node in enumerate(neighbour_list[i]):
+            for idx, neighbour_node in enumerate(self.neighbour_list[i]):
                 L[:, cnt_col+idx:cnt_col+idx+1] /= demands[neighbour_node]            
             cnt_col += n_neighbour[i]
 
--- run_this.py
+++ run_this.py
@@ -98,9 +98,9 @@ if __name__ == "__main__":
     # buffer_size: 128(should be small, since lower-level agents change policies);   
     input_args = ['--algorithm_name', algo, '--seed', '35', '--mode', 'train',   
                 #   '--is_two_loop',  
-                  '--episode_length', '10', '--min_bonus', '0', '--max_bonus', '4', '--lr_drivers', '5e-1',   
-                  '--lr', '1e-4', '--tau', '5e-3', '--buffer_size', '128', '--batch_size', '16',   
-                  '--warmup_steps', '100', '--num_env_steps', '4000',  
+                  '--episode_length', '1', '--min_bonus', '0', '--max_bonus', '4', '--lr_drivers', '5e-1',   
+                  '--lr', '1e-4', '--tau', '5e-3', '--buffer_size', '128', '--batch_size', '2',   
+                  '--warmup_steps', '10', '--num_env_steps', '4000',  
                   '--min_epsilon', '0', '--max_epsilon', '0.2', '--decre_epsilon_episodes', '4000']
                   # "is_two_loop" with this tag, the environment would run in two loops, outer loop would wait until inner loop reach equilibrium. 
 
